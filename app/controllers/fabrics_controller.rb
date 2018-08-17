95f2986865082021e75be10b0d535d364ee3da37
Grace Averis <graceaveris@gmail.com>
2018-08-17 11:23:21 +0800

    a few days work...

--- app/controllers/fabrics_controller.rb
+++ app/controllers/fabrics_controller.rb
@@ -7,15 +7,41 @@ class FabricsController < ApplicationController
 
   def index
     @fabrics = Fabric.where(["user_id = ?", current_user.id])
-    #for add fabric modal
+    #arrays for add fabric modal
     @fibres = ["Silk", "Cotton", "Linen", "Wool", "Merino", "Modal", "Bamboo", "Cashmere", "Polyester", "Acetate", "Nylon", "Lurex"]
     @suitable_for = ["Pants", "Skirt", "Shirt", "Dress", "T-Shirt", "Shorts", "Activewear", "Sweater", "Jacket", "Coat", "Swimwear"]
     @colours = ["Red", "Blue", "Orange", "Yellow", "Green", "Purple", "Black", "Grey", "Brown", "Pink", "Metallic"]
   end
 
-   def filter
-    @fabrics = Fabric.print_scope(params[:filter][:printed])
+
+  def filter
+    #This puts all fabrics through through various filters, if filter submitted.
     @filtered = true
+    @fabrics = Fabric.where(["user_id = ?", current_user.id])
+    @fabrics = @fabrics.with_colour(params[:filter][:colour]) if params[:filter][:colour].present?
+    @fabrics = @fabrics.with_fibre(params[:filter][:fibre]) if params[:filter][:fibre].present?
+    @fabrics = @fabrics.suitable_for_scope(params[:filter][:suitable_for]) if params[:filter][:suitable_for].present?
+ 
+    #for pre-selection based on prior search. For pre-selects we need to if/else for the defauls. Although there must be a tidier way to do this!!!
+    #DEFINES FILTER BAR PRE-SETS
+   if (params[:filter][:fibre]) == ""
+      @selected_fibre = "Fibre Type"
+    else
+      @selected_fibre = (params[:filter][:fibre])
+   end
+
+   if (params[:filter][:suitable_for]) == ""
+      @selected_suitable_for = "Suitable For"
+    else
+      @selected_suitable_for = (params[:filter][:suitable_for])
+   end
+
+   if (params[:filter][:colour]) == ""
+    @selected_colour = "Select Colour"
+    else
+    @selected_colour = (params[:filter][:colour])
+   end
+    
     #for add fabric modal
     @fibres = ["Silk", "Cotton", "Linen", "Wool", "Merino", "Modal", "Bamboo", "Cashmere", "Polyester", "Acetate", "Nylon", "Lurex"]
     @suitable_for = ["Pants", "Skirt", "Shirt", "Dress", "T-Shirt", "Shorts", "Activewear", "Sweater", "Jacket", "Coat", "Swimwear"]
--- app/models/fabric.rb
+++ app/models/fabric.rb
@@ -1,6 +1,10 @@
 class Fabric < ApplicationRecord
     belongs_to :user
-    scope :print_scope, -> (print) { where("printed = ? ", true) }
-    scope :with_colour, -> (colour) { where("colour = ?", colour) }
-    
+    scope :print_scope, -> (print) { where("printed = ? ", true) } #NOT IN USE
+    scope :plain_scope, -> (print) { where("printed = ? ", false) } #NOT IN USE
+    scope :with_colour, -> (colour) { where(":colour = ANY(colour)", colour: colour ) }
+    scope :suitable_for_scope, -> (suitable_for) { where(":suitable_for = ANY(suitable_for)", suitable_for: suitable_for ) }
+    scope :with_fibre, -> (fibre) { where(":fibre = ANY(fibre)", fibre: fibre ) }
 end
+
+
