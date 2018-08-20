103fa4775e40a849e94aeb67a6bd6acdfb2ca8c3
Grace Averis <graceaveris@gmail.com>
2018-08-20 12:12:45 +0800

    second basic test working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,21 @@ require "rails_helper"
 RSpec.describe User, type: :model do
 
   before(:each) do
-    @user = User.create!(username: "Test", email: "steven@gmail.com", password: "hello")
+    @user = User.create!(username: "Test", email: "grace@gmail.com", password: "hello")
   end
 
-  describe "validation" do
+#positive testing // PASS
+  describe "new user validation" do
     it "should let a user be created with inputs" do
       expect(@user).to be_valid
+    end
+  end
+
+#positive testing // PASS
+ describe "email validation" do
+      it "should make sure a user has not already created an account" do
+      @newuser = User.create(username: "Test", email: "grace@gmail.com", password: "hello")
+      expect(@newuser).to_not be_valid
     end
   end
 
