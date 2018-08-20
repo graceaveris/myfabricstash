c2cfcd58588a58ee52c71b06ecaded4753cf6327
Grace Averis <graceaveris@gmail.com>
2018-08-20 12:44:23 +0800

    another test added, but failing need to fix a few things

---
+++ spec/features/create_fabric_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+feature "Create fabric", type: :feature do
+
+  before(:each) do
+    @user = User.create!(username: "Grace", email: "grace@gmail.com", password: "password")
+    visit sign_in_path
+    fill_in "email", with: "grace@gmail.com"
+    fill_in "password", with: "password"
+    click_on "Submit"
+  end
+
+
+# POSITIVE TEST // FAILING
+
+describe "creating fabrics" do
+  it "should create a fabric" do
+    visit "/fabrics/collection"
+    expect(page).to have_content "YOUR FABRIC COLLECTION"
+    click_button "Add Fabric to collection"
+    fill_in "fabric[fabric_name]", with: "test"
+    fill_in "fabric[fabric_description]", with: "test"
+    click_button "Add Fabric"
+    expect(page).to have_content "Welcome"
+  end
+end
+  
+end
